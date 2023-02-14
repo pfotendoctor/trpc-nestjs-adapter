c55131b1fe371e15b8e990e65430c693e1a6ae55
tillpd <till.wierbach@tierarztpluspartner.de>
2023-02-14 17:46:27 +0100

    change createContext signiture

--- src/lib/trpc-module-options.type.ts
+++ src/lib/trpc-module-options.type.ts
@@ -1,7 +1,9 @@
 import { AnyRouter } from "@trpc/server";
+import { Request } from '@nestjs/common';
+import { NestResolver } from "./nest-resolver.type";
 
 export interface TrpcModuleOptions<TRouter = AnyRouter> {
   path: "/trpc" | string;
   router: TRouter;
-  createContext: (...args: any[]) => any | Promise<any>;
+  createContext: (request: Request, resolveNestDependency: NestResolver) => any | Promise<any>;
 }
